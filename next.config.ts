51737faed3475362abb6429cae1f8f37fe9e8120
Lizijian <3161584612@qq.com>
2025-10-11 13:23:18 +0800

    build: add basePath in next.config.ts to adapt the cloud server

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/english-ideas",
   output: "standalone",
   serverExternalPackages: ["@prisma/client", "prisma"],
 };
